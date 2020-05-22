f228f7ef325db661d6f7d6dd66dc5575c79397ab
jason <libingnan@sdiread.com>
2020-05-23 01:15:43 +0800

    修改密码

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'lbn0628...',
-  database: 'my_app',
-  port: 3306
+    host: '59.110.24.86',
+    user: 'root',
+    password: 'lbn0628...',
+    database: 'my_app',
+    port: 3306,
+    charset: 'utf8'
 });
 
 connection.connect();
